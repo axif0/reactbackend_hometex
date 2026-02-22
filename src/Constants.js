137be705a60d52b7cfae5c986bc25e3dc837cb78
axif <muhamadasif570@gmail.com>
2026-02-23 04:06:50 +0600

    refactor: update AddCustomer and ShowOrderConfirmation modals for improved readability and maintainability

--- src/Constants.js
+++ src/Constants.js
@@ -1,7 +1,7 @@
 const Constants = {
-    BASE_URL : window.location.hostname === 'localhost' ? 'http://localhost:8000/api': 'https://htbapi.hometexbd.ltd/api'
+    // BASE_URL : window.location.hostname === 'localhost' ? 'http://localhost:8000/api': 'https://htbapi.hometexbd.ltd/api'
     // BASE_URL : 'https://htbapi.hometexbd.ltd/api'
-    // BASE_URL : 'https://api.hometexbangladesh.org/api'
+    BASE_URL : 'https://api.hometexbangladesh.org/api'
 }
 
 export default Constants;
